61b5f2f8ae4158613b861b31dbccd28716cf5adc
Michal Guzy <michalguzym@gmail.com>
2022-07-25 11:25:27 +0100

    Created the initial file structure for the UserManagementAPI

---
+++ src/SwiftUserManagement/Controllers/SwiftUserManagementController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwiftUserManagement.Entities;
+using SwiftUserManagement.Repositories;
+using System.Net;
+
+namespace SwiftUserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwiftUserManagementController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public SwiftUserManagementController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(User), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<User>> createUser()
+        {
+
+        }
+    }
+}
---
+++ src/SwiftUserManagement/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwiftUserManagement.Entities;
+
+namespace SwiftUserManagement.Repositories
+{
+
+    public interface IUserRepository 
+    {
+        Task<User> GetUser(string userName);
+        Task<User> CreateUser(User user);
+        Task<User> UpdateUser(User user);
+    }
+}
