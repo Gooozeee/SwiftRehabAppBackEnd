304643806fbac0e1a9b4242d798be7b3110f2a63
Michal Guzy <michalguzym@gmail.com>
2022-07-25 16:55:05 +0100

    Added extra entities to allow for storing them in the database

---
+++ src/SwiftUserManagement.API/Entities/Video.cs
@@ -0,0 +1,11 @@
+﻿namespace SwiftUserManagement.API.Entities
+{
+    // Entity which is used for showing where the video data is stored
+    public class Video
+    {
+        public int Video_Id { get; set; }
+        public int User_Id { get; set; }
+        public string FilePath { get; set; }
+        public string Prediction { get; set; }
+    }
+}
