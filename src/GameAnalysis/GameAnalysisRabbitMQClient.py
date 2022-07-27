957121d03e8f271f97d8a42945a77e3dc5e5069d
Michal Guzy <michalguzym@gmail.com>
2022-07-27 15:34:47 +0100

    Fixed bug where logs didn't show up in docker container

--- src/GameAnalysis/GameAnalysisRabbitMQClient.py
+++ src/GameAnalysis/GameAnalysisRabbitMQClient.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
 import pika
+import time
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq'))
-channel = connection.channel
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+    analyseData(body)
+
+
+# Any data analysis will be done in this method
+def analyseData(body):
+    print("Data is being analysed: " + str(body))
+
+
+time.sleep(20)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+channel = connection.channel()
 
 channel.exchange_declare(exchange='swift_rehab_app', exchange_type='topic')
 
@@ -13,10 +24,7 @@ queue_name = result.method.queue
 
 channel.queue_bind(exchange='swift_rehab_app', queue=queue_name, routing_key='game.score.fromApp')
 
-print(' [*] Waiting for tasks. To exit press CTRL+C')
-
-def callback(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body))
+print(' [*] Waiting for tasks.')
 
 channel.basic_consume(queue = queue_name, on_message_callback=callback, auto_ack=True)
 
